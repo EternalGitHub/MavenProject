7af077dead6bb0f15ed4daba9f72eac8e8707474
EternalGitHub <axe9906@gmail.com>
2022-08-27 22:58:03 -0400

    Updated

--- src/test/java/Test/DemoTest.java
+++ src/test/java/Test/DemoTest.java
@@ -9,12 +9,18 @@ public class DemoTest
 	@BeforeMethod
 	public void setUp()
 	{
+		
 		System.out.println("Before Method Execute..");
 	}
 	
 	@Test
 	public void LoginTest()
 	{
+		int i = 5;
+		for(int k=0;k<i;k++)
+		{
+			System.out.println(k);
+		}
 		System.out.println("Login Test");
 	}
 	
